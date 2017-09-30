1fb0af2e1631ef8f380298f219f3d08140c12c38
topeas <peiqixin@gmail.com>
2017-09-30 15:30:20 +0800

    添加默认的账号

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,67 +1,95 @@
-const User = require ('../models/user')
+const User = require('../models/user')
 import { generateToken } from '../middlewares/jwt'
 import CURD from './curd'
 
+const admin = {
+  username: 'admin',
+  password: 'dev',
+  email: 'peiqixin@gmail.com'
+}
+
 export const add = async (ctx, next) => {
-  const { username, password, email, description, } = ctx.request.body
-  if (!username) return ctx.error ({ message: '用户名不存在' })
-  if (!password) return ctx.error ({ message: '密码不存在' })
-  if (!email) return ctx.error ({ message: '必须需要邮箱' })
-  const _findUser = await User.findOne ({
+  const { username, password, email, description } = ctx.request.body
+  if (!username) return ctx.error({ message: '用户名不存在' })
+  if (!password) return ctx.error({ message: '密码不存在' })
+  if (!email) return ctx.error({ message: '必须需要邮箱' })
+  const _findUser = await User.findOne({
     username
   })
-  if (_findUser) return ctx.error ('用户名已经存在了', {}, 400)
-  const user = await User.create ({ username, password, email, description, createAt: new Date () })
-  ctx.success ('创建成功', user, 201)
-  await next ()
+  if (_findUser) return ctx.error('用户名已经存在了', {}, 400)
+  const user = await User.create({
+    username,
+    password,
+    email,
+    description,
+    createAt: new Date()
+  })
+  ctx.success('创建成功', user, 201)
+  await next()
 }
 
 export const login = async (ctx, next) => {
   const { username, password } = ctx.request.body
-  const user = await User.findOne ({ username })
-  if (!user) return ctx.error ('用户不存在')
+  if (username === admin.username && password === admin.password) {
+    const token = generateToken({ username, email: admin.email })
+    ctx.success('登录成功', { token })
+    return (ctx.session = {
+      username: admin.username,
+      email: admin.email,
+      role: 'admin',
+      lastLoginDate: new Date()
+    })
+  }
+  const user = await User.findOne({ username })
+
+  if (!user) return ctx.error('用户不存在')
   if (password === user.password) {
-    const token = generateToken ({ username, email: user.email })
-    ctx.success ('登录成功', { token })
+    const token = generateToken({ username, email: user.email })
+    ctx.success('登录成功', { token })
     ctx.session = {
       username,
       email: user.email,
       role: 'admin',
-      lastLoginDate: new Date (),
+      lastLoginDate: new Date()
     }
   } else {
-    return ctx.error ('密码错误', {})
+    return ctx.error('密码错误', {})
   }
-  await next ()
+  await next()
 }
 
 export const all = async (ctx, next) => {
-  const users = await User.find ()
-  if (users) return ctx.success ('获取所有数据成功', { users }, 200)
-  await next ()
+  const users = await User.find()
+  if (users) return ctx.success('获取所有数据成功', { users }, 200)
+  await next()
 }
 
 export const del = async (ctx, next) => {
   const _id = ctx.params.id
-  if (!_id) return ctx.error ('没有id', {}, 404)
-  const user = await User.findByIdAndRemove ({
+  if (!_id) return ctx.error('没有id', {}, 404)
+  const user = await User.findByIdAndRemove({
     _id
   })
-  if (user) return ctx.success ('删除成功', {}, 200)
-  ctx.error ('删除失败', {}, -1)
+  if (user) return ctx.success('删除成功', {}, 200)
+  ctx.error('删除失败', {}, -1)
 }
 
 export const one = async (ctx, next) => {
   const _id = ctx.params.id
-  if (!_id) return ctx.error ('没有id', {}, 404)
-  const user = await User.findOne ({
+  if (!_id) return ctx.error('没有id', {}, 404)
+  const user = await User.findOne({
     _id
   })
-  if (user) return ctx.success ('获取单个用户成功', {
-    username: user.username,
-    email: user.email,
-  }, 200)
-  ctx.error ('不存在此用户', {}, 200)
+  if (user)
+    return ctx.success(
+      '获取单个用户成功',
+      {
+        username: user.username,
+        email: user.email
+      },
+      200
+    )
+  ctx.error('不存在此用户', {}, 200)
 }
 
 // 测试redis。第一次使用redis
@@ -80,22 +108,20 @@ export const test = async (ctx, next) => {
   //   }
   //
   // }
-  ctx.success ('访问接口成功')
+  ctx.success('访问接口成功')
 }
 
-export const testClass = async (ctx) => {
-
+export const testClass = async ctx => {
   class UserClass extends CURD {
     constructor() {
-      super ()
+      super()
     }
 
     static isTest(ctx) {
-      return super.isHaveAuth (ctx)
+      return super.isHaveAuth(ctx)
     }
-
   }
 
   // const cc = new UserClass ()
-  return ctx.success (UserClass.isTest (ctx))
+  return ctx.success(UserClass.isTest(ctx))
 }
